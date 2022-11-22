1cdce8898e21e22fb39909f262e5cc119c301e18
lsa0828 <lsa0828@duksung.ac.kr>
2022-11-22 17:37:02 +0900

    댓글 수정 기능 구현하기

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
@@ -11,6 +11,7 @@ from django.shortcuts import get_object_or_404
 class PostUpdate(LoginRequiredMixin,UpdateView):
     model = Post
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category'] # , 'tags'
+    # 템플릿 post_forms
 
     template_name = 'blog/post_update_form.html'
 
@@ -51,6 +52,7 @@ class PostUpdate(LoginRequiredMixin,UpdateView):
 class PostCreate(LoginRequiredMixin,UserPassesTestMixin,CreateView):
     model = Post
     fields = ['title','hook_text','content','head_image','file_upload','category'] # ,'tags'
+    # 템플릿 post_forms
 
     def test_func(self):
         return self.request.user.is_superuser or self.request.user.is_staff
@@ -145,6 +147,19 @@ def new_comment(request, pk):
             return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    # CreateView, UpdateView, form을 사용하면
+    # 템플릿 모델명_forms : comment_form
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate,self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
 
     # 템플릿 모델명_detail.html : post_detail.html
     # 파라미터 모델명 : post
